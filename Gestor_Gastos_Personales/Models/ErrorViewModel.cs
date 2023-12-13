0da6e2c0acdc798cf89dd4f9d57958acdc002e7a
SilRC <silrc2005@gmail.com>
2023-12-13 04:27:51 -0600

    Creando tablas sql

---
+++ Gestor_Gastos_Personales/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gestor_Gastos_Personales.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
