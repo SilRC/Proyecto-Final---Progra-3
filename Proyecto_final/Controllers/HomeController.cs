91745dd6111599827bd315197c5dd76c5f063f6e
SilRC <silrc2005@gmail.com>
2023-12-17 02:27:38 -0600

    Pruebas y cambios css

--- Proyecto_final/Controllers/HomeController.cs
+++ Proyecto_final/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Proyecto_final.Models;
 using System.Diagnostics;
 
@@ -6,10 +7,14 @@ namespace Proyecto_final.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ProyectoFinalDbContext _context;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+       
+        public HomeController(ProyectoFinalDbContext context, ILogger<HomeController> logger)
         {
+            _context = context;
             _logger = logger;
         }
 
@@ -22,6 +27,70 @@ namespace Proyecto_final.Controllers
         {
             return View();
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Buscar el usuario en la base de datos por nombre de usuario y contraseña
+                var usuario = await _context.Usuarios
+                    .FirstOrDefaultAsync(u => u.NombreUsuario == model.NombreUsuario && u.Contrasena == model.Contrasena);
+
+                if (usuario != null)
+                {
+                    // Usuario autenticado, redirige a la página de inicio
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    // Usuario no encontrado, agrega un error al modelo
+                    ModelState.AddModelError(string.Empty, "Nombre de usuario o contraseña incorrectos.");
+                }
+            }
+
+            // Si el modelo no es válido o la autenticación falló, vuelve a la vista de inicio de sesión con errores
+            return View();
+        }
+
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Registro(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                // Aquí deberías guardar el usuario en la base de datos
+                // Puedes utilizar Entity Framework Core o cualquier otro ORM
+
+                // Por ejemplo, si estás utilizando EF Core:
+                // dbContext.Usuarios.Add(usuario);
+                // dbContext.SaveChanges();
+
+                _context.Add(usuario);
+                await _context.SaveChangesAsync();
+
+                // Redirige a la página de inicio de sesión o cualquier otra página
+                return RedirectToAction("Login", "Home");
+            }
+
+            // Si el modelo no es válido, vuelve a la vista de registro con errores
+            return View();
+        }
+
+        public IActionResult Informes()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ Proyecto_final/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Proyecto_final.Models
+{
+    public class LoginModel
+    {
+       public string NombreUsuario { get; set; }
+        public string Contrasena { get; set; }
+    }
+}
